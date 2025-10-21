1d1cda73a0358a355563036e822962de24a24bab
Palash Pidkewar <pidkewarp@gmail.com>
2025-10-21 14:19:35 +0530

    v

--- src/app/utils/axiosInstance.js
+++ src/app/utils/axiosInstance.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:"https://rechargebackendnew.onrender.com"
+  baseURL:"https://rechargefrontend.onrender.com"
+  
  
 
 });
