1838c952bc968f4c86b537ef6000ce35592bd81e
Palash Pidkewar <pidkewarp@gmail.com>
2025-10-23 21:25:47 +0530

    Describe your changes here

--- src/app/utils/axiosInstance.js
+++ src/app/utils/axiosInstance.js
@@ -2,14 +2,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
- 
-  baseURL: "rechargebackendnew-pvbf.vercel.app",
- 
-
+  baseURL: "https://rechargebackendnew-1.onrender.com",
 });
- //  baseURL: "https://recharge-cu59.onrender.com",
-//   baseURL: "http://localhost:5000",
-// baseURL:"https://rechargefrontend.onrender.com"
+
 axiosInstance.interceptors.request.use((config) => {
   if (typeof window !== "undefined") {
     const token = localStorage.getItem("token");
